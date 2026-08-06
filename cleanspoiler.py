ca955f507f2b5e559af422a8136040aab1876509
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-08-07 00:10:10 +0800

    Implement spoiler cleanup script for Wikipedia

---
+++ cleanspoiler.py
@@ -0,0 +1,14 @@
+import pywikibot as pwb
+import time
+def main(times):
+  Site = pwb.Site("zh", "wikipedia")
+  t = 0
+  for page in site.search('insource:"id=spoiler style="', namespaces=(1, 4), content=True):
+    page.text = page.text.replace('id="spoiler" style="', \
+          'style="border-top: 2px solid var(--border-color-base, #a2a9b1); border-bottom: 2px solid var(--border-color-base, #a2a9b1) ')
+    page.save(summary="清理#spoiler", minor=true, bot=true)
+    t += 1
+    if t >= times:
+      return
+    time.sleep(5)
+main()

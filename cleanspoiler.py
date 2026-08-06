6404c6123f23eadd484df7519b7ef076789ad328
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-08-07 00:45:59 +0800

    Update cleanspoiler.py

--- cleanspoiler.py
+++ cleanspoiler.py
@@ -3,7 +3,7 @@ import time
 def main(times):
   Site = pwb.Site("zh", "wikipedia")
   t = 0
-  for page in site.search(r'insource:/id\s*=\s*\"spoiler\"\s*style\s*=/', namespaces=(1, 4), content=True):
+  for page in site.search(r'insource:/id\s*=\s*\"spoiler\"\s*style\s*=/', namespaces=(1, 4, 5), content=True):
     page.text = page.text.replace('id="spoiler" style="', \
           'style="border-top: 2px solid var(--border-color-base, #a2a9b1); border-bottom: 2px solid var(--border-color-base, #a2a9b1); ')
     page.save(summary="清理#spoiler", minor=true, bot=true)

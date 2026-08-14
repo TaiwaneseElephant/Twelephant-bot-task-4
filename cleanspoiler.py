3628b3f749d0529bc09e1a96c56c15399c1c1286
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-08-14 17:24:41 +0800

    Update cleanspoiler.py

--- cleanspoiler.py
+++ cleanspoiler.py
@@ -7,5 +7,5 @@ def main():
     page.text = page.text.replace('id="spoiler" style="', \
           'style="border-top: 2px solid var(--border-color-base, #a2a9b1); border-bottom: 2px solid var(--border-color-base, #a2a9b1); ')
     page.save(summary="（[[Wikipedia:机器人/申请/Twelephant-bot/3|BRFA]]）清理#spoiler", minor=true, bot=true)
-    time.sleep(300)
+    time.sleep(10)
 main()

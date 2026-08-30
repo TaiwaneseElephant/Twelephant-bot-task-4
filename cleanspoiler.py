2f1ad70c50cb903b8efcff510f1d94e4e9a90c66
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-08-30 19:43:37 +0800

    Update cleanspoiler.py

--- cleanspoiler.py
+++ cleanspoiler.py
@@ -2,7 +2,7 @@ import pywikibot as pwb
 import time
 import re
 def main():
-  Site = pwb.Site("zh", "wikipedia")
+  site = pwb.Site("zh", "wikipedia")
   for page in site.search(r'insource:/id\s*=\s*\"spoiler\"\s*style\s*=/', namespaces=(1, 4, 5), content=True):
     page.text = re.sub(r'id\s*=\s*\"spoiler\"\s*style\s*=/"', \
           'style="border-top: 2px solid var(--border-color-base, #a2a9b1); border-bottom: 2px solid var(--border-color-base, #a2a9b1); ', \

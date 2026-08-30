000160d27d9975ec3734f8e5a7f222e170ee2c4f
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-08-30 20:38:28 +0800

    Update cleanspoiler.py

--- cleanspoiler.py
+++ cleanspoiler.py
@@ -1,18 +1,14 @@
 import pywikibot as pwb
 import time
 import re
-def main(times):
+def main():
   site = pwb.Site("zh", "wikipedia")
-  t=46
   for page in site.search(r'insource:/id\s*=\s*\"spoiler\"\s*style\s*=\s*\"/', namespaces=(1, 4, 5), content=True):
     newtext = re.sub(r'id\s*=\s*"spoiler"\s*style\s*=\s*"', \
           'style="border-top: 2px solid var(--border-color-base, #a2a9b1); border-bottom: 2px solid var(--border-color-base, #a2a9b1); ', \
                      page.text)
     if newtext != page.text:
       page.text = newtext
-      t += 1
       page.save(summary=("[[User:Twelephant-bot/task/4|Task 4]]：清理#spoiler"), minor=True, bot=True)
-      if t == times:
-        break
     time.sleep(10)
-main(50)
+main()
